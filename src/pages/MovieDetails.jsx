cc02840f826c1405e53e7abf3c97853b3af41ebf
jmlinares2019 <jlinaresmar@gmail.com>
2024-09-21 23:18:11 +0200

    Fetching and showing movie data (only structure, no style yet)

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -6,24 +6,59 @@ const MovieDetails = () => {
     const { id } = useParams(); 
     const [movieDetails, setMovieDetails] = useState({});
 
+    // fetching movie data
     useEffect(() => {
         async function fetchMovieDetails(id){
             console.log(id);
             try{
-            const res = await fetch(`http://www.omdbapi.com/?apikey=df6ac670&i=${id}`);
-            const data = await res.json();
-            console.log(data);
-            setMovieDetails(data);
-            console.log(movieDetails);  
+                const res = await fetch(`http://www.omdbapi.com/?apikey=df6ac670&i=${id}&plot=full`);
+                const data = await res.json();
+                console.log(data);
+                setMovieDetails(data);
             } catch(error) {
-            console.log(error);
+                console.log(error);
             }
         }
+        
         fetchMovieDetails(id);
     }, []);
 
     return (
-        <div>MovieDetails {id}</div> 
+        <div className="container justify-content-start">
+            <div className="row title-wrapper">
+                <div className="col-auto">
+                    <h1 className="movie-title">{movieDetails.Title}</h1>
+                </div>
+            </div>
+            <div className="row basic-metadata-wrapper">
+                <div className="col-auto">
+                    <span>{movieDetails.Year} · </span>
+                    <span>{movieDetails.Runtime} · </span>
+                    <span>{movieDetails.Type}</span>
+                </div>
+            </div>
+            <div className="row img-plot-wrapper">
+                <div className="col-6 img-col">
+                    <img src={movieDetails.Poster} alt={`${movieDetails.Title} (${movieDetails.Year})`} />
+                </div>
+                <div className="col-6 contents-col">
+                    <div className="genres">
+                    {/* Genres come in a string, but we need them as an array */}
+                    {(movieDetails.Genre.split(",")).map((genre) => (
+                        <span className="genre">{genre}</span>
+                    ))}
+                    </div>
+                    <p className="plot">{movieDetails.Plot}</p>
+                    <div className="ratings">
+                        <span className="rating">{`${movieDetails.imdbRating}/10`} | </span>
+                        <span className="ratings-count">{movieDetails.imdbVotes}</span>
+                    </div>
+                    <p className="director">Director: {movieDetails.Director}</p>
+                    <p className="writers">Writers: {movieDetails.Writer}</p>
+                    <p className="cast">Cast: {movieDetails.Actors}</p>
+                </div>
+            </div>
+        </div> 
     )
 }
 
